3878c73421a7a4e0d81b1953d10729bf63d16bdb
Nya Garcia <nyablk97@gmail.com>
2020-04-18 20:12:46 +0200

    feat(main.module): add GoogleMapsModule

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { MapComponent } from './components/map/map.component';
 import { MainComponent } from './main.component';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { CommonModule } from '@angular/common';
 
 const components = [MainComponent, MapComponent];
 
 @NgModule({
   declarations: [...components],
-  imports: [SharedModule],
+  imports: [SharedModule, GoogleMapsModule],
   providers: [],
   exports: [...components]
 })
