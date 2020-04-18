5e14bc3ab83b6a092324ab72918125e3d72f7d6c
Nya Garcia <nyablk97@gmail.com>
2020-04-18 19:06:49 +0200

    feat(app.module): add Shared and Pages modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { NgModule } from '@angular/core';
+import { PagesModule } from './pages/pages.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GoogleMapsModule],
+  imports: [SharedModule, PagesModule, BrowserModule, GoogleMapsModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
