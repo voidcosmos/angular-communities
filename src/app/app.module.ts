6d362b60f15a1e972c2578f1929ae04e7f5dea18
Nya Garcia <nyablk97@gmail.com>
2020-04-18 20:11:32 +0200

    refactor(app.module): remove maps module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { GoogleMapsModule } from '@angular/google-maps';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { PagesModule } from './pages/pages.module';
@@ -8,13 +7,7 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    SharedModule,
-    PagesModule,
-    BrowserModule,
-    GoogleMapsModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, SharedModule, PagesModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
