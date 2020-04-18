535d95e7e6638f64c587453d1a134d11bfc34240
Nya Garcia <nyablk97@gmail.com>
2020-04-18 20:12:22 +0200

    feat(map): add map component logic and html

--- src/app/pages/main/components/map/map.component.ts
+++ src/app/pages/main/components/map/map.component.ts
@@ -1,4 +1,6 @@
-import { OnInit, Component } from '@angular/core';
+import { OnInit, Component, ViewChild } from '@angular/core';
+import { MapMarker, GoogleMap, MapInfoWindow } from '@angular/google-maps';
+import { CommunityService } from 'app/community.service';
 
 @Component({
   selector: 'ngcommunity-map',
@@ -6,5 +8,75 @@ import { OnInit, Component } from '@angular/core';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-  ngOnInit() {}
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
+  @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow;
+
+  markers = [];
+  zoom = 11;
+  center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    // mapTypeId: 'hybrid',
+    maxZoom: 15,
+    minZoom: 8
+  };
+  infoContent = '';
+
+  constructor(private communityService: CommunityService) {}
+
+  ngOnInit() {
+    const communities$ = this.communityService.communities;
+    communities$.subscribe(console.log);
+    navigator.geolocation.getCurrentPosition(position => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+    });
+  }
+
+  zoomIn() {
+    if (this.zoom < this.options.maxZoom) {
+      this.zoom++;
+    }
+  }
+
+  zoomOut() {
+    if (this.zoom > this.options.minZoom) {
+      this.zoom--;
+    }
+  }
+
+  click(event: google.maps.MouseEvent) {
+    console.log(event);
+  }
+
+  logCenter() {
+    console.log(JSON.stringify(this.map.getCenter()));
+  }
+
+  addMarker() {
+    this.markers.push({
+      position: {
+        lat: this.center.lat + ((Math.random() - 0.5) * 2) / 10,
+        lng: this.center.lng + ((Math.random() - 0.5) * 2) / 10
+      },
+      label: {
+        color: 'red',
+        text: 'Marker label ' + (this.markers.length + 1)
+      },
+      title: 'Marker title ' + (this.markers.length + 1),
+      info: 'Marker info ' + (this.markers.length + 1),
+      options: {
+        animation: google.maps.Animation.BOUNCE
+      }
+    });
+  }
+
+  openInfo(marker: MapMarker, content) {
+    this.infoContent = content;
+    this.info.open(marker);
+  }
 }
