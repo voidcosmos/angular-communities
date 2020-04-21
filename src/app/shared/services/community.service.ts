45182dbbf212b2684e9487b763a56deaf14575a5
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2020-04-21 19:00:55 +0200

    feat(community.service): NormalizedCommunity

--- src/app/shared/services/community.service.ts
+++ src/app/shared/services/community.service.ts
@@ -2,16 +2,27 @@ import { Communities } from '@shared/interfaces';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommunityService {
   JSON_COMMUNITIES = 'assets/json/communities.json';
+  DEFAULT_IMAGE = 'assets/images/0.png';
 
   constructor(private httpClient: HttpClient) {}
 
   get communities(): Observable<Communities> {
-    return this.httpClient.get<Communities>(this.JSON_COMMUNITIES);
+    return this.httpClient
+      .get<Communities>(this.JSON_COMMUNITIES)
+      .pipe(map(communities => this.normalizeCommunities(communities)));
+  }
+  private normalizeCommunities(communities: Communities): Communities {
+    const communitiesNormalized = Object.entries(communities).map(([title, community]) => {
+      const image = !community.image ? this.DEFAULT_IMAGE : community.image;
+      return [title, { ...community, image }];
+    });
+    return Object.fromEntries(communitiesNormalized);
   }
 }
