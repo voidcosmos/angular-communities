8a82112639855cd8078463be455caffa6e71929e
tonivj5 <tonivj5@gmail.com>
2020-04-25 19:42:24 +0200

    fix: improve ui and search

--- src/app/pages/main/components/community-preview/community-preview.component.ts
+++ src/app/pages/main/components/community-preview/community-preview.component.ts
@@ -1,5 +1,7 @@
 import { Communities, Community } from '@shared/interfaces';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'ngcommunity-preview',
@@ -12,7 +14,29 @@ export class CommunityPreviewComponent {
   @Output()
   selected = new EventEmitter<Community>();
 
+  filter = new FormControl();
+
+  filtered$ = this.filter.valueChanges.pipe(
+    startWith(''),
+    map((value: string) => value.toLowerCase()),
+    map(value => this.getFilteredCommunitiesBy(value)),
+  );
+
   onSelected(selected: string) {
     this.selected.emit(this.communities[selected]);
+  }
+
+  private getFilteredCommunitiesBy(criteria: string) {
+    return Object.values(this.communities).filter(
+      ({ name, city }) => this.contains(name, criteria) || this.contains(city, criteria),
+    );
+  }
+
+  private contains(value: string, contained: string) {
+    return value
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .toLowerCase()
+      .includes(contained);
   }
 }
