3fbde9bd4637473d41fbfad884122d158b831ba9
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2020-05-02 00:19:06 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { NgModule } from '@angular/core';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, PagesModule, HttpClientModule, RouterModule.forRoot([])],
+  imports: [BrowserModule, SharedModule, PagesModule, HttpClientModule, RouterModule.forRoot([]), ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
