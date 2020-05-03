acbf1aa4b8899dbba48b1af9c09087f0263844f0
tonivj5 <tonivj5@gmail.com>
2020-05-03 19:51:59 +0200

    feat(workflow): check communities json against schema

---
+++ check-communities.js
@@ -0,0 +1,25 @@
+const AJV = require('ajv');
+const { $schema: _, ...communities } = require('./src/assets/json/communities.json');
+const schema = require('./src/assets/json/schema.json');
+
+const ajv = new AJV({ allErrors: true });
+
+ajv.addKeyword('unique-community-id', {
+  compile: () => {
+    const ids = new Set();
+
+    return id => {
+      if (ids.has(id)) {
+        return false;
+      }
+
+      ids.add(id);
+
+      return true;
+    };
+  },
+});
+
+if (!ajv.validate(schema, communities)) {
+  throw ajv.errors;
+}
