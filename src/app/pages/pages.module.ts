370214d52593e1d004a59dac373cb056adc6d7e2
Nya Garcia <nyablk97@gmail.com>
2020-06-24 18:49:37 +0200

    feat(pages.module): import FaqModule

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { MainModule } from './main/main.module';
+import { FaqModule } from './faq/faq.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
-  imports: [SharedModule, MainModule],
+  imports: [SharedModule, MainModule, FaqModule],
   providers: [],
   exports: [MainModule],
 })
