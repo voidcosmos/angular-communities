f50d25411d62f73d0903dbd18cbaa5ccf778c23f
Nya Garcia <nyablk97@gmail.com>
2020-06-24 18:47:59 +0200

    feat(main.component): add communities logic

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Community, Communities } from '@shared/interfaces';
 import { Component, Input } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { CommunityService } from '@shared/services';
 
 @Component({
   selector: 'ngcommunity-main',
@@ -10,11 +11,17 @@ import { map } from 'rxjs/operators';
 })
 export class MainComponent {
   @Input()
-  communities: Communities;
+  /* communities: Communities; */
+  communitie$ = this.communityService.communities;
+  community$ = this.route.fragment.pipe(map(community => this.communitie$[community]));
 
-  community$ = this.route.fragment.pipe(map(community => this.communities[community]));
-
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private communityService: CommunityService,
+  ) {
+    console.log(this.communitie$);
+  }
 
   changeCommunity(community?: Community) {
     this.router.navigate([], {
