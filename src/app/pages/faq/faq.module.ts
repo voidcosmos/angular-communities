32a3833ec124bc2578862144fd23d2dc917fa0b5
Nya Garcia <nyablk97@gmail.com>
2020-06-26 13:33:16 +0200

    refactor(faq.module): remove unused modules

--- src/app/pages/faq/faq.module.ts
+++ src/app/pages/faq/faq.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
 import { FaqComponent } from './faq.component';
 
 const COMPONENTS = [FaqComponent];
 
 @NgModule({
-  declarations: [...COMPONENTS],
-  imports: [CommonModule, SharedModule, FlexLayoutModule, ReactiveFormsModule, MatInputModule],
-  exports: [...COMPONENTS],
+  declarations: [COMPONENTS],
+  imports: [CommonModule, SharedModule, FlexLayoutModule],
+  exports: [COMPONENTS],
 })
 export class FaqModule {}
