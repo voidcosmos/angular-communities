1072eadd86ac0949c000248e09f4d2934e95ab56
koushik <koushik.ramaswamy@apitive.com>
2020-06-30 12:47:15 +0530

    refactor(share): add snackbar

--- src/app/pages/main/components/sidenav/sidenav.component.ts
+++ src/app/pages/main/components/sidenav/sidenav.component.ts
@@ -1,6 +1,7 @@
 import { Communities, Community } from '@shared/interfaces';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, TemplateRef, ViewChild } from '@angular/core';
 import { Clipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'ngcommunity-sidenav',
@@ -8,7 +9,7 @@ import { Clipboard } from '@angular/cdk/clipboard';
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent {
-  constructor(private clipboard: Clipboard) {}
+  constructor(private clipboard: Clipboard, private snackBar: MatSnackBar) {}
   @Input() community: Community;
   @Input() communities: Communities;
 
@@ -16,6 +17,7 @@ export class SidenavComponent {
   selected = new EventEmitter<Community>();
   @Output()
   closeInfo = new EventEmitter<void>();
+  @ViewChild('linkCopied') copied: TemplateRef<any>;
 
   onSelected(selected: Community) {
     this.selected.emit(selected);
@@ -34,11 +36,18 @@ export class SidenavComponent {
           url: location.href,
         })
         .then(() => console.log('Successful share'))
-        .catch(error => console.log('Error sharing', error));
+        .catch(error => console.error('Error sharing', error));
     }
   }
 
   onCopy() {
     this.clipboard.copy(location.href);
+    this.snackBar.openFromTemplate(this.copied, {
+      duration: 2000
+    });
+  }
+
+  onCloseSnackBar() {
+    this.snackBar.dismiss();
   }
 }
