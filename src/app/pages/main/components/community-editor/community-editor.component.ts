99d5aafe1e0ae055c343c07915010ff8a19220a0
Rubén Triviño Juárez <ruben@willimbu.com>
2020-07-03 13:01:18 +0200

    Navigate to editor view on buttoon click
    Updates side-nav fragment on route change to /#add_community

---
+++ src/app/pages/main/components/community-editor/community-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngcommunity-editor',
+  templateUrl: './community-editor.component.html',
+  styleUrls: ['./community-editor.component.scss'],
+})
+export class CommunityEditorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -5,8 +5,15 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { CommunityPreviewComponent } from './components/community-preview/community-preview.component';
+import { CommunityEditorComponent } from './components/community-editor/community-editor.component';
 
-const COMPONENTS = [MainComponent, MapComponent, SidenavComponent, CommunityPreviewComponent];
+const COMPONENTS = [
+  MainComponent,
+  MapComponent,
+  SidenavComponent,
+  CommunityPreviewComponent,
+  CommunityEditorComponent,
+];
 
 @NgModule({
   declarations: COMPONENTS,
