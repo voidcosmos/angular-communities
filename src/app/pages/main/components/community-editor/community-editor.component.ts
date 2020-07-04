abd4c6dc8d4a5217b8d7e171fdb89e9d62b89a3d
Rubén Triviño Juárez <ruben@willimbu.com>
2020-07-04 18:44:31 +0200

    add reactive dynamic form group with basic validation

--- src/app/pages/main/components/community-editor/community-editor.component.ts
+++ src/app/pages/main/components/community-editor/community-editor.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'ngcommunity-editor',
@@ -6,7 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./community-editor.component.scss'],
 })
 export class CommunityEditorComponent implements OnInit {
-  constructor() {}
+  @Output()
+  closeInfo = new EventEmitter<void>();
+
+  public communityForm: FormGroup;
+  constructor(public formBuilder: FormBuilder) {
+    this.communityForm = formBuilder.group({
+      communityName: ['', [Validators.required]],
+      location: ['', Validators.required],
+      description: ['', Validators.required], // ADD LENGTH VALIDATOR WITH COUNTER AT THE BOTTOM
+      city: ['', Validators.required],
+      twitter: ['', Validators.required],
+      web: ['', Validators.required],
+      youtube: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      organizers: formBuilder.array([this.createOrganizerFromGroup()]),
+    });
+    console.log(this.communityForm);
+  }
 
   ngOnInit(): void {}
+
+  createOrganizerFromGroup() {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      twitter: ['', Validators.required],
+      github: [''],
+    });
+  }
+  addOrganizer() {
+    (this.communityForm.get('organizers') as FormArray).push(this.createOrganizerFromGroup());
+  }
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.communityForm.value);
+  }
+
+  onCloseInfo() {
+    this.closeInfo.emit();
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { MainModule } from './main/main.module';
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
-  imports: [SharedModule, MainModule],
+  imports: [SharedModule, MainModule, ReactiveFormsModule],
   providers: [],
   exports: [MainModule],
 })
