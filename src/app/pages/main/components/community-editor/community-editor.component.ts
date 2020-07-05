e8b0d5d11e8506313bfcbe80741b27c8813bd7e8
Rubén Triviño Juárez <ruben@willimbu.com>
2020-07-05 15:48:10 +0200

    add Webs options input to Organizer formGroup

--- src/app/pages/main/components/community-editor/community-editor.component.ts
+++ src/app/pages/main/components/community-editor/community-editor.component.ts
@@ -72,30 +72,48 @@ export class CommunityEditorComponent implements AfterViewInit {
       name: ['', Validators.required],
       twitter: ['', Validators.required],
       github: [''],
+      webs: this.formBuilder.array([this.formBuilder.control('')]),
     });
   }
   addOrganizer() {
     (this.communityForm.get('organizers') as FormArray).push(this.createOrganizerFromGroup());
   }
+  addWeb(organizerIndex: number) {
+    (this.communityForm.get('organizers')['controls'][organizerIndex]['controls'][
+      'webs'
+    ] as FormArray).push(this.formBuilder.control(''));
+  }
   onSubmit() {
-    let formResult = { ...this.communityForm.value };
-    formResult['name'] = this.autocompletedInput;
-    formResult['id'] = this.lastCommunityId + 1;
-
-    // Remove empty optional inputs
-    formResult.organizers.forEach(organizer => {
-      if (organizer.github === '') delete organizer.github;
-    });
-    delete formResult.addressName;
-    if (formResult.web === '') delete formResult.web;
-    if (formResult.youtube === '') delete formResult.youtube;
-
-    // TODO: Use EventEmitter with form value
+    let cleanedFormResult = this.cleanFormResult(this.communityForm.value);
     let copyOperation = this.clipboard.beginCopy(
-      `"${formResult.name}": ` + JSON.stringify(formResult),
+      `"${cleanedFormResult.name}": ` + JSON.stringify(cleanedFormResult),
     );
     copyOperation.copy();
     copyOperation.destroy();
+  }
+
+  cleanFormResult(formResult: any) {
+    // This functions should be tested
+    let cleanedResult = { ...formResult };
+    cleanedResult['name'] = this.autocompletedInput;
+    cleanedResult['id'] = this.lastCommunityId + 1;
+
+    // Remove empty optional inputs
+    cleanedResult.organizers.forEach(organizer => {
+      if (organizer.github === '') delete organizer.github;
+      if (organizer.webs && organizer.webs.length === 0) delete organizer.webs;
+      else if (!organizer.webs) {
+        delete organizer.webs;
+      } else {
+        organizer.webs = organizer.webs.filter(web => web != '');
+        if (organizer.webs.length === 0) delete organizer.webs;
+      }
+    });
+    delete cleanedResult.addressName;
+    if (cleanedResult.web === '') delete cleanedResult.web;
+    if (cleanedResult.youtube === '') delete cleanedResult.youtube;
+
+    return cleanedResult;
   }
 
   onCloseInfo() {
